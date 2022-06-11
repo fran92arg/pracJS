8898ff081e46920d138ec639619dc372690092b2
Francisco <francis_oli92@hotmail.com>
2022-06-11 18:14:34 -0300

    resolucoon item 5

--- practica1.js
+++ practica1.js
@@ -1,16 +1,21 @@
+
+//ejercicio 1
 function suma( a, b){
   c=a+b;
   return c;
 }
-console.log(suma);
+//console.log(suma);
+//ejercicio 2
 function presentacion(a,b){
     return("Hola,soy "+a+" y tengo "+b+" años")
 }
-console.log(presentacion("Francisco","29"));
+//console.log(presentacion("Francisco","29"));
+//ejercicio 3
 function potencia(){
     return (Math.pow(2,3));
 }
-console.log(potencia());
+//console.log(potencia());
+//ejercicio 4
 function idioma(nombre){
     if(nombre=="Español"){
         return("Hola");
@@ -20,7 +25,34 @@ function idioma(nombre){
         return("Ola");
     }else return("Error, ingrese un idioma válido")
 }
-console.log(idioma("Español"));
+//console.log(idioma("Español"));
+
+/*ejercicio 5
+item ---> 5
+
+function sumar(a, b){
+    return a + b;
+}
+console.log(sumar(10, 2));
+console.log()
+console.log(restar(5, 2));
+var restar = function(a, b){
+    return a - b;
+}
+Analiza la función y si hay algún error, arregla el hilo de ejecución.
+Muestra los valores.*/
+//solucion
+function sumar(a, b){
+    return a + b;
+}
+console.log(sumar(10, 2));
+console.log()
+var restar = function(a, b){
+    return a - b;
+}
+console.log(restar(5, 2));
+
+ 
 //let array=[10,21,22,2,5];
 
 function sacaMayores(arreglo){
